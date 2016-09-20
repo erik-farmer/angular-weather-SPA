27ccb4c5cf10437615b983d88c653f07c56c2fd4
erik-farmer <erikdfarmer@gmail.com>
2016-09-20 09:46:21 -0700

    Convert from angular router to ui-router

--- app/routes.js
+++ app/routes.js
@@ -4,22 +4,33 @@
         .module('weatherApp')
         .config(config);
 
-    function config($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'home/home.html',
-                controller: 'homeController',
-                controllerAs: 'vm'
-            })
-            .when('/forecast', {
-                templateUrl: 'forecast/forecast.html',
-                controller: 'forecastController',
-                controllerAs: 'vm'
-            })
-            .when('/forecast/:days', {
-                templateUrl: 'forecast/forecast.html',
-                controller: 'forecastController',
-                controllerAs: 'vm'
-            });
+    function config($stateProvider) {
+
+        var homeState = {
+            name: 'home',
+            url: '/',
+            controller: 'homeController',
+            controllerAs: 'vm',
+            templateUrl: 'home/home.html'
+        };
+
+        var forecastState = {
+            name: 'forecast',
+            url: '/forecast',
+            controller: 'forecastController',
+            controllerAs: 'vm',
+            templateUrl: 'forecast/forecast.html'
+        };
+
+        var forecastDaysState = {
+            name: 'forecastDays',
+            url: '/forecast/{days}',
+            controller: 'forecastController',
+            controllerAs: 'vm',
+            templateUrl: 'forecast/forecast.html'
+        };
+        $stateProvider.state(homeState);
+        $stateProvider.state(forecastState);
+        $stateProvider.state(forecastDaysState);
     }
 })();
