dcc87457b5c6d2062f6874764605ebd87a3eafb0
erik-farmer <erikdfarmer@gmail.com>
2016-09-21 11:02:51 -0700

    Chain states based on PR feedback

--- app/routes.js
+++ app/routes.js
@@ -1,36 +1,33 @@
 // Routes
 (function () {
+
     angular
         .module('weatherApp')
         .config(config);
 
     function config($stateProvider) {
 
-        var homeState = {
-            name: 'home',
-            url: '/',
-            controller: 'homeController',
-            controllerAs: 'vm',
-            templateUrl: 'home/home.html'
-        };
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'homeController',
+                controllerAs: 'vm',
+                templateUrl: 'home/home.html'
+            })
+            .state('forecast', {
+                url: '/forecast',
+                controller: 'forecastController',
+                controllerAs: 'vm',
+                templateUrl: 'forecast/forecast.html'
+            })
+            .state('forecastDays', {
+                name: 'forecastDays',
+                url: '/forecast/{days}',
+                controller: 'forecastController',
+                controllerAs: 'vm',
+                templateUrl: 'forecast/forecast.html'
+            });
 
-        var forecastState = {
-            name: 'forecast',
-            url: '/forecast',
-            controller: 'forecastController',
-            controllerAs: 'vm',
-            templateUrl: 'forecast/forecast.html'
-        };
-
-        var forecastDaysState = {
-            name: 'forecastDays',
-            url: '/forecast/{days}',
-            controller: 'forecastController',
-            controllerAs: 'vm',
-            templateUrl: 'forecast/forecast.html'
-        };
-        $stateProvider.state(homeState);
-        $stateProvider.state(forecastState);
-        $stateProvider.state(forecastDaysState);
     }
+
 })();
